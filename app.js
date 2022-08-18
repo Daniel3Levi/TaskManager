ab549d814ceaccba9fd8d49176073bd923c56e47
Daniel <daniel3levi@gmail.com>
2022-08-18 13:47:13 +0300

    test

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.use(express.static('./public'));
 app.use(express.json());
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 // routes
 
